8df232754ca28fa58e4e49db8dce60a38f85c030
Guilherme Furlaneto <gpfurlaneto@gmail.com>
2022-01-30 19:01:54 -0300

    api url env key

--- src/lib/api-config.ts
+++ src/lib/api-config.ts
@@ -1,5 +1,8 @@
 export const apiConfig = {
-  baseUrl: () => process.env.API_BASE_URL || 'http://localhost:4000/api',
+  baseUrl: () => {
+    console.log(process.env.API_BASE_URL)
+    return process.env.API_BASE_URL || 'http://localhost:4000/api'
+  },
   session: {
     signUp: () => '/session/sign-up',
     signIn: () => '/session/login'
