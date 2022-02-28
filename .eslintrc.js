f758cc1267a3b50af7e130683625d43e1f4cac34
Guilherme Furlaneto <furlaneto@Guilhermes-MacBook-Pro.local>
2022-02-28 13:39:31 -0300

    lint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,69 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+    'plugin:import/typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'prettier'
+  ],
+  rules: {
+    'no-console': 0,
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'linebreak-style': ['error', 'unix'],
+    'global-require': 0,
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { ignoreRestSiblings: true },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      }
+    ],
+    'class-methods-use-this': 0,
+    'import/prefer-default-export': 0,
+    'no-useless-constructor': 0,
+    'no-empty-function': 0,
+    'max-len': 0,
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+  },
+  settings: {
+    react: { version: 'detect' },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', './'],
+      },
+    },
+  },
+};
